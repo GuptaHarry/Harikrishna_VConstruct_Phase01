84c2552a2eb540c9e1548b16a9f86d9fc3a4b444
Harikrishna Gupta <harikrishnag@vconstruct.in>
2026-01-27 15:52:34 +0530

    Validation Form question solution with modification added

--- JS_Practice/Question1_DOM.js
+++ JS_Practice/Question1_DOM.js
@@ -1,10 +1,8 @@
 const form = document.getElementById("registerForm");
 
-
-
 function isUsernameValid(value) {
-                                          // Only letters, minimum 3 characters
-  const usernameRegex = /^[A-Za-z]{3,}$/; 
+  // Only letters, minimum 3 characters
+  const usernameRegex = /^[A-Za-z]{3,}$/;
   return usernameRegex.test(value);
 }
 
@@ -14,22 +12,21 @@ function isEmailValid(value) {
 }
 
 function validatePassword(value) {
-
-    // implementation of Password Policy 
+  // implementation of Password Policy
   const rules = {
     lower: /[a-z]/.test(value),
     upper: /[A-Z]/.test(value),
     number: /[0-9]/.test(value),
     special: /[^A-Za-z0-9]/.test(value),
-    length: value.length >= 8
+    length: value.length >= 8,
   };
-                                       
-  updateRuleUI("lower", rules.lower);       // here automatically the function are called for the return values.
+
+  updateRuleUI("lower", rules.lower); // here automatically the function are called for the return values.
   updateRuleUI("upper", rules.upper);
   updateRuleUI("number", rules.number);
   updateRuleUI("special", rules.special);
-  updateRuleUI("length", rules.length); 
-  
+  updateRuleUI("length", rules.length);
+
   const resultAfterChange = Object.values(rules); // gives us array of[ true , false , true , false]. etc
   // check if all true are present otherwise fasle.
 
@@ -48,7 +45,6 @@ function updateRuleUI(id, isValid) {
     el.className = "invalid-rule";
   }
 }
-
 
 // main function of validation
 
@@ -80,7 +76,7 @@ function validateInput(input) {
   return valid;
 }
 
-// Vlaidation for every real-time Change using event bubbling . 
+// Vlaidation for every real-time Change using event bubbling .
 
 form.addEventListener("input", (event) => {
   const element = event.target;
@@ -95,14 +91,14 @@ form.addEventListener("submit", (event) => {
   const inputs = form.querySelectorAll("input");
   let formIsValid = true;
 
-  inputs.forEach(input => {
+  inputs.forEach((input) => {
     if (!validateInput(input)) {
       formIsValid = false;
     }
   });
 
   if (!formIsValid) {
-    event.preventDefault();   // default browser is to refresh page once submitted , to avoid that we can use preventDefault.
+    event.preventDefault(); // default browser is to refresh page once submitted , to avoid that we can use preventDefault.
     alert("Please fix the errors before submitting.");
   } else {
     alert("Form submitted successfully!");
